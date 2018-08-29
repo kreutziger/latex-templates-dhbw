760f8adb6c8c947c08a92e7e92a0c002f4b7ba95
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2018-08-29 15:01:24 +0200

    use custom content directory

--- make.sh
+++ make.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
-cd "$(dirname "$0")/$1"
+
+# $1 - template name
+# $2 - content directory (optional)
+
+cd "$(dirname "$0")"
+
+TEMPLATE=$1
+WRKDIR=`pwd`
+if [ -z "$2" ]; then
+    CONTENTDIR="$WRKDIR/sample-content"
+else
+    CONTENTDIR=$2
+fi
+
+export TEXINPUTS="$CONTENTDIR:$TEXINPUTS"
+
+cd $TEMPLATE
 ./make.sh
-mv document.pdf ../$1.pdf
+mv document.pdf $CONTENTDIR/$TEMPLATE.pdf
 
 #cleanup
 rm document.aux
--- research-project/make.sh
+++ research-project/make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pdflatex -halt-on-error document
-bibtex document
-pdflatex -halt-on-error document
-pdflatex -halt-on-error document
+pdflatex -halt-on-error ./document
+bibtex ./document
+pdflatex -halt-on-error ./document
+pdflatex -halt-on-error ./document
